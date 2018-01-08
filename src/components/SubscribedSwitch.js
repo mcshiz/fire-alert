da14af362acc9ca5361ceaa576740bd40bef1506
Brian McCall <mccabj0210@gmail.com>
2018-01-08 12:51:32 -0800

    change button to subscribed switch

--- src/components/SubscribedSwitch.js
+++ src/components/SubscribedSwitch.js
@@ -1,27 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import '../styles/SubscribedSwitch.css'
 
 class SubscribedSwitch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubscribedChange = this.handleSubscribedChange.bind(this);
-    }
-
-    handleSubscribedChange = (e) => {
-        let subscribed = !this.props.subscribed;
-        this.props.changeSubscribed(subscribed)
-    };
     render() {
+        let subscribed = this.props.subscribed ? "Subscribed" : "Unsubscribed";
         return (
             <div className="subscribed-switch-container text-right">
-                <span className="subscribed-switch-label">Subscribed</span>
+                <span className="subscribed-switch-label">{subscribed}</span>
                 <label className="switch">
-                    <input type="checkbox"  checked={this.props.subscribed} onChange={()=> this.handleSubscribedChange()} />
+                    <input type="checkbox"  checked={this.props.subscribed} onChange={()=> this.props.toggleSubscribe()} />
                         <span className="slider round" />
                 </label>
             </div>
         )
     }
 }
+
+SubscribedSwitch.propTypes = {
+    subscribed: PropTypes.bool,
+    toggleSubscribe: PropTypes.func.isRequired
+};
 export default SubscribedSwitch
