892b6e62c18e36d5587931d977c697a449028670
Brian McCall <mccabj0210@gmail.com>
2018-01-07 16:05:14 -0800

    change welcome prop name; add auth actions and auth

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as DashboardActions from '../actions/dashboard';
-import * as FireListActions from '../actions/fireList';
-import * as UserActions from '../actions/user'
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
 
-import FireList from './FireList.js'
-import Welcome from '../components/Welcome.js'
-import DashboardMap from '../components/DashboardMap.js'
+import * as DashboardActions from "../actions/dashboard";
+import * as FireListActions from "../actions/fireList";
+import * as AuthActions from "../actions/auth";
+import * as UserActions from "../actions/user";
 
+import Welcome from "../components/Welcome";
+import FireList from './FireList'
+import DashboardMap from '../components/DashboardMap'
 
 class Dashboard extends React.Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.props.authAction.checkToken();
         this.props.fireListActions.loadActiveFiresList();
-        this.props.userActions.loadUserInfo()
+        this.props.userActions.loadUserInfo();
     }
-
     render() {
         return (
-        <div className="col-xs-12 col-md-10 col-md-offset-1">
-            <Welcome user={this.props.user.first_name} />
-            <FireList />
-            <DashboardMap options={this.props.mapOptions} fires={this.props.activeFires} {...this.props} />
-        </div>
+            !this.props.isFetching &&
+            <div className="col-xs-12 col-md-10 col-md-offset-1">
+                <Welcome name={this.props.user.first_name} />
+                <FireList />
+                <DashboardMap options={this.props.mapOptions} fires={this.props.activeFires} {...this.props} />
+            </div>
         )
     };
 }
@@ -31,7 +34,9 @@ function mapStateToProps(state, prop) {
     return {
         mapOptions: state.dashboard.mapOptions,
         activeFires: state.fireList.activeFires,
-        user: state.user.user
+        user: state.user.user,
+        isFetching: state.auth.isFetching,
+        authenticated: state.auth.authenticated,
     }
 }
 
@@ -39,7 +44,8 @@ function mapDispatchToProps(dispatch){
     return {
         dashboardActions: bindActionCreators(DashboardActions, dispatch),
         fireListActions: bindActionCreators(FireListActions, dispatch),
-        userActions: bindActionCreators(UserActions, dispatch)
+        userActions: bindActionCreators(UserActions, dispatch),
+        authAction: bindActionCreators(AuthActions, dispatch)
     }
 }
 
