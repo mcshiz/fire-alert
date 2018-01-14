ed2ec808b735454c4e8175b3d1016d6ca5f6af5b
Brian McCall <mccabj0210@gmail.com>
2018-01-13 16:12:39 -0800

    move buttons to next row

--- src/components/AddRecipient.js
+++ src/components/AddRecipient.js
@@ -5,27 +5,31 @@ class AddRecipient extends React.Component {
     render() {
         return (
             <form className="form-inline" action="/recipients" method='post' onSubmit={this.props.addRecipient.bind(this)}>
-                <li className='recipient-item col-xs-12'>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="first_name" name="first_name"/>
+                <li className='recipient-item'>
+                    <div className="col-xs-12">
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="first_name" name="first_name"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="last_name" name="last_name"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="phone_number" name="phone_number"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="email" className="form-control" id="email"  name="email" />
+                        </div>
+                        <div className="form-group">
+                            <select name="status" id="status" className="form-control">
+                                <option value={true}>Enabled</option>
+                                <option value={false}>Disabled</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="last_name" name="last_name"/>
-                    </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="phone_number" name="phone_number"/>
-                    </div>
-                    <div className="form-group">
-                        <input type="email" className="form-control" id="email"  name="email" />
-                    </div>
-                    <div className="form-group">
-                        <select name="status" id="status" className="form-control">
-                            <option value={true}>Enabled</option>
-                            <option value={false}>Disabled</option>
-                        </select>
-                    </div>
-                    <div className="form-group pull-right">
-                        <button type='submit' className='btn btn-success'>Add <i className='fa fa-check'></i></button>
+                    <div className="col-xs-12">
+                        <div className="form-group">
+                            <button type='submit' className='btn btn-success'>Add <i className='fa fa-check'></i></button>
+                        </div>
                     </div>
                 </li>
             </form>
--- src/components/ListRecipients.js
+++ src/components/ListRecipients.js
@@ -6,29 +6,33 @@ class ListRecipients extends React.Component {
 
     render() {
         let Recipients = this.props.recipients.map((recipient, idx) => (
-            <li className='recipient-item col-xs-12' key={idx}>
+            <li className='recipient-item ' key={idx}>
                 <form className="form-inline" onSubmit={this.props.applyEdits.bind(this, recipient)} method='put' name={idx}>
-                    <div className="form-group">
-                            <input type="text" className="form-control" id="first_name" value={recipient.first_name} onChange={this.props.editRecipient.bind(this, recipient)}/>
+                    <div className="col-xs-12">
+                        <div className="form-group">
+                                <input type="text" className="form-control" id="first_name" value={recipient.first_name} onChange={this.props.editRecipient.bind(this, recipient)}/>
+                        </div>
+                        <div className="form-group ">
+                            <input type="text" className="form-control" id="last_name" value={recipient.last_name} onChange={this.props.editRecipient.bind(this, recipient)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="phone_number" value={recipient.phone_number}onChange={this.props.editRecipient.bind(this, recipient)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="email" className="form-control" id="email" value={recipient.email} onChange={this.props.editRecipient.bind(this, recipient)} />
+                        </div>
+                        <div className="form-group">
+                            <select name="status" id="status" className="form-control " value={recipient.status} onChange={this.props.editRecipient.bind(this, recipient)}>
+                                <option value={true}>Enabled</option>
+                                <option value={false}>Disabled</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="form-group ">
-                        <input type="text" className="form-control" id="last_name" value={recipient.last_name} onChange={this.props.editRecipient.bind(this, recipient)} />
-                    </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="phone_number" value={recipient.phone_number}onChange={this.props.editRecipient.bind(this, recipient)} />
-                    </div>
-                    <div className="form-group">
-                        <input type="email" className="form-control" id="email" value={recipient.email} onChange={this.props.editRecipient.bind(this, recipient)} />
-                    </div>
-                    <div className="form-group">
-                        <select name="status" id="status" className="form-control " value={recipient.status} onChange={this.props.editRecipient.bind(this, recipient)}>
-                            <option value={true}>Enabled</option>
-                            <option value={false}>Disabled</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <button type='submit' disabled={true} className='btn btn-primary submit' ><i className='fa fa-check'></i></button>
-                        <button type='button' className='btn btn-danger remove' onClick={this.props.deleteRecipient.bind(this, recipient)}><i className='fa fa-trash'></i></button>
+                    <div className="col-xs-12">
+                        <div className="form-group">
+                            <button type='submit' disabled={true} className='btn btn-primary submit' >Save <i className='fa fa-check'></i></button>
+                            <button type='button' className='btn btn-danger remove' onClick={this.props.deleteRecipient.bind(this, recipient)}>Delete <i className='fa fa-trash'></i></button>
+                        </div>
                     </div>
                 </form>
             </li>
